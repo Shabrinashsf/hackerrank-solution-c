77919b956d7577d51e4848faf6505d60c9c9fe6b
Shabrina Amalia Safaana <shabrinaamalia860@gmail.com>
2024-07-27 14:33:21 +0700

    Update and rename plus-minus to plus-minus.c

--- plus-minus.c
+++ plus-minus.c
@@ -1,3 +1,24 @@
+/* 
+Task:
+1. Declare 4 variables: two of type int and two of type float.
+2. Read 2 lines of input from stdin (according to the sequence given in the 'Input Format' section below) and initialize your  variables.
+3. Use the + and - operator to perform the following operations:
+4. Print the sum and difference of two int variable on a new line.
+5. Print the sum and difference of two float variable rounded to one decimal place on a new line.
+
+Constraints
+1 <= integer variables <= 10000
+1 <= float variables <= 10000 
+
+Input:
+10 4
+4.0 2.0
+
+Output:
+14 6
+6.0 2.0
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
